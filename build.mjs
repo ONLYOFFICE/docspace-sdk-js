4b2cd8611119ba766e7834df17f6a371d00beefd
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2024-09-25 16:39:02 +0300

    SDK: Fixed build runner

---
+++ build.mjs
@@ -0,0 +1,29 @@
+import { build } from "esbuild";
+import { dtsPlugin } from "esbuild-plugin-d.ts";
+
+const option = {
+  bundle: true,
+  color: true,
+  logLevel: "info",
+  sourcemap: false,
+  entryPoints: ["./src/main.ts"],
+  minify: true,
+};
+
+async function run() {
+  await build({
+    format: "esm",
+    outdir: "dist",
+    splitting: true,
+    plugins: [dtsPlugin()],
+    ...option,
+  }).catch(() => process.exit(1));
+
+  await build({
+    format: "cjs",
+    outfile: "./dist/api.js",
+    ...option,
+  }).catch(() => process.exit(1));
+}
+
+run();
