a0a778f2247d3c2878eefc13a823d150e664f69e
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2024-10-17 14:58:35 +0300

    SDK: Fixed default export for sdk class

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import SDK from "./sdk";
-import SDKInstance from "./instance";
-export * from "./types";
-export * from "./enums";
-export * from "./constants";
-export * from "./utils";
 
 window.DocSpace = window.DocSpace || {};
 
 window.DocSpace.SDK = window.DocSpace.SDK || new SDK();
 
-export { SDK, SDKInstance };
+export default SDK;
