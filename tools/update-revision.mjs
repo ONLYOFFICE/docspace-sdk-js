b5a918760eca6cdde040e79c1fc73eabeefdd3c3
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2025-11-24 13:59:01 +0300

    Fixed update revision script

--- tools/update-revision.mjs
+++ tools/update-revision.mjs
@@ -12,6 +12,11 @@ try {
 
   const config = JSON.parse(readFileSync(CONFIG_FILE, "utf-8"));
 
+  if (config.gitRevision === gitBranch) {
+    console.log(`Revision already set to: ${gitBranch}`);
+    process.exit(0);
+  }
+
   config.gitRevision = gitBranch;
 
   writeFileSync(CONFIG_FILE, `${JSON.stringify(config, null, 2)}\n`, "utf-8");
